e66eaf60208161eb40795dafb1fb51043d5ccabf
Clayton <claytonsibanda60@gmail.com>
2018-10-12 01:00:02 +0200

    all works now!!!!!!!!!!!!!!

--- main.cpp
+++ main.cpp
@@ -29,22 +29,24 @@
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[i].size();j++){
                // cout<<"Max is: "<<max<<endl;
+                max=-100;
+                for(char action:actions) {
+                    double reward = 0;
 
-                for(char action:actions){
-                    double reward=0;
 
-
-                    if(action=='D' && i==1 && j==2)
-                        reward=50;
-                    if(action =='R' && i==0 && j==1) {
-                        reward =   100.0;
+                    if (action == 'D' && i == 1 && j == 2)
+                        reward = 100.0;
+                    if (action == 'R' && i == 0 && j == 1) {
+                        reward = 50.0;
                     }
-                  //specify for the policy state
-                  if(i==0 && j==2)
-                      policy=0;
-                  else
-                      policy= reward+discount_factor*(grid[i][j].do_action(grid,i,j,action,iteration));
+                    //specify for the policy state
+                    if (i == 0 && j == 2)
+                        policy = 0;
+                    else {
 
+                    double val = discount_factor * (grid[i][j].do_action(grid, i, j, action, iteration));
+                    policy = reward+val;
+                }
 
 
                     if(policy>max){
@@ -60,11 +62,16 @@
         }
    // cout<<"The max is: "<<max<<endl;
     iteration++;
-if(iteration==101){
+//if(iteration==4){
+//
+//    break;
+//}
 
+
+if(isStop(grid)){
+    cout<<"number of iterations: "<<iteration-1<<endl;
     break;
 }
-
     }
 
 
@@ -79,20 +86,17 @@ if(iteration==101){
 
 
     bool isStop(vector<vector<state> > &grid){
-        cout<<"istop top"<<endl;
+
+        bool stop =true;
         for (auto &row : grid) {
 
             for(int j=0;j< row.size();j++){
-                if(row[j].getPolicies()[row[j].getPolicies().size()-1]== row[j].getPolicies()[row[j].getPolicies().size()-2]){
-                    cout<<"istop inside1"<<endl;
-
-                    return true;
+                if(row[j].getPolicies()[row[j].getPolicies().size()-1]!= row[j].getPolicies()[row[j].getPolicies().size()-2]){
+                    stop=false;
                 }
-                cout<<"istop inside2"<<endl;
             }
         }
-        cout<<"istop bottom"<<endl;
 
-        return false;
+        return  stop;
 
     }
---
+++ readME.md
@@ -0,0 +1,25 @@
+## LAB 8  solution by Clayton Sibanda
+
+
+Question 1: How many iterations does it take for the Value Iteration algorithm
+to converge? In an output text file list the optimal values (V
+∗for eachstate).
+
+Answer:
+4 iterations
+
+Question 2: Assume we start in state s1, give the states that form the
+optimal policy (π∗) to reach the terminal state (s3).
+
+Answer:
+S1,S2,S5,S6,S3
+
+
+Question 3: Is it possible to change the reward function function so that
+V∗ changes, but the optimal policy (π∗) remains unchanged?
+
+Answer:
+: Yes. Double each each immediate reward. Then V∗ is also doubled and π ∗ remains unchanged
+
+
+
