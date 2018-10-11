58a204e461febf34170d7e252d4521c475bf1b0b
Clayton <claytonsibanda60@gmail.com>
2018-10-11 11:48:49 +0200

    create state class and needed methods

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include "state.h"
-
+#include <vector>
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int discount_factor=0.8;
+
+
+
+    vector<vector<state> > grid;
+
+    for(int i=0;i<2;i++){
+
+        for(int j=0;j<3;j++){
+            state s(0);
+            vector<state> v;
+            v.push_back(s);
+            grid.push_back(v);
+        }
+
+    }
+
+
     return 0;
 }
--- state.h
+++ state.h
@@ -5,9 +5,29 @@
 #ifndef REINFORCEMENTLEARNING_STATE_H
 #define REINFORCEMENTLEARNING_STATE_H
 
-
+#include <vector>
+#include <string>
 class state {
 
+    double optimal_policy;
+    std::vector<double> policies;
+
+
+
+public:
+    state(double _optimal_policy):optimal_policy(_optimal_policy){  }
+
+    double getOptimal_policy() const {
+        return optimal_policy;
+    }
+
+    const std::vector<double> &getPolicies() const {
+        return policies;
+    }
+
+    double do_action(std::vector<std::vector<state>> grid,int i,int j,std::string direction ){
+
+    }
 
 };
 
