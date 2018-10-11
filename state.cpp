1dca35c2376aef49789834f03af5afc78e665ca1
Clayton <claytonsibanda60@gmail.com>
2018-10-11 10:43:40 +0200

    first commit

---
+++ state.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lenovo-User on 2018/10/11.
+//
+
+#include "state.h"
---
+++ state.h
@@ -0,0 +1,15 @@
+//
+// Created by Lenovo-User on 2018/10/11.
+//
+
+#ifndef REINFORCEMENTLEARNING_STATE_H
+#define REINFORCEMENTLEARNING_STATE_H
+
+
+class state {
+
+
+};
+
+
+#endif //REINFORCEMENTLEARNING_STATE_H
